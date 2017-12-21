42f4ad13d464aa465c757c454408a71160c1a9f5
parkpoom <parkpoom.d@gmail.com>
2017-12-21 14:05:23 +0700

    setup google api autenticate and create file config

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://<dbuser>:<dbpassword>@ds143081.mlab.com:43081/storybooks-dev',
+  googleClientID: '528102445248-jcrbot3ihjnmp9lusknf0jaaqqmju2bh.apps.googleusercontent.com',
+  googleClientSecret: 'DFYeDgjowHmPb0W-bulkdmnb'
+}
