bdc61498c350fa1ab3b4f30274c8dcdd7ee3343c
parkpoom <parkpoom.d@gmail.com>
2017-12-21 16:04:18 +0700

    create model user

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ require('./config/passport')(passport);
 // Load Routes
 const auth = require('./routes/auth');
 
+// Load Keys
+const keys = require('./config/keys');
+
+// Map global promises
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, {
+  useMongoClient: true
+}).then(() => {
+  console.log('MongoDB Connected');
+}).catch((err) => {
+  console.log(err)
+});
+
 const app = express();
 
 app.get('/', (req, res) => {
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-  mongoURI: 'mongodb://<dbuser>:<dbpassword>@ds143081.mlab.com:43081/storybooks-dev',
+  mongoURI: 'mongodb://deer:1234@ds143081.mlab.com:43081/storybooks-dev',
+  // mongoURI: 'mongodb://localhost:27017/storybooks-dev',
   googleClientID: '528102445248-jcrbot3ihjnmp9lusknf0jaaqqmju2bh.apps.googleusercontent.com',
   googleClientSecret: 'DFYeDgjowHmPb0W-bulkdmnb'
 }
