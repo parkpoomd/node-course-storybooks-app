36e5fa61e895ec26a4fd05cbfca9bbc5dc82ca1c
parkpoom <parkpoom.d@gmail.com>
2017-12-23 23:12:17 +0700

    file config for production

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://deer:1234@ds143081.mlab.com:43081/storybooks-dev',
-  // mongoURI: 'mongodb://localhost:27017/storybooks-dev',
-  googleClientID: '528102445248-jcrbot3ihjnmp9lusknf0jaaqqmju2bh.apps.googleusercontent.com',
-  googleClientSecret: 'DFYeDgjowHmPb0W-bulkdmnb'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
